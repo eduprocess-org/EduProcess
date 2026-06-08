97eb8cb2ba0ae8eea5752784449aa008009f5335
xFrixonL <134994364+xFrixonL@users.noreply.github.com>
2026-06-07 19:47:03 -0500

    fix(frontend): update api client endpoint layout to support reverse proxy routes (EDUPR-61)

--- frontend/src/services/api/apiClient.ts
+++ frontend/src/services/api/apiClient.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("API URL =", import.meta.env.VITE_API_URL);
+
 export const apiClient = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
   headers: {
