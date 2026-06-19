0ab027ed61c3de5ebb2a3b680b2abc542efbb24d
Vanessa Heredia <123616810+vnheredia@users.noreply.github.com>
2026-06-19 14:28:19 -0500

    feat(admin): implement administrative observations form and history timeline (EDUPR-160) (#118)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "sonner";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AuthProvider }from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
 import './styles/index.css'
 import App from './App.tsx'
 
