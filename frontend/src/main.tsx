ca0aa4ee2f84354184e55a9c439b65d98ba9d6e6
Vanessa Heredia <vanessaheredia444@gmail.com>
2026-06-24 10:28:43 -0500

    style(admin): remove remaining git merge conflict markers (EDUPR-228)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
-<<<<<<< HEAD
 
 import { AuthProvider } from "./context/AuthContext";
-
-import "./styles/index.css";
 import App from "./App";
+import "./styles/index.css";
 
 const queryClient = new QueryClient();
 
@@ -17,29 +15,7 @@ createRoot(document.getElementById("root")!).render(
       <AuthProvider>
         <App />
       </AuthProvider>
-
       <Toaster richColors position="top-right" />
     </QueryClientProvider>
   </StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'  
-import { AuthProvider } from "./context/AuthContext";
-import './styles/index.css'
-import App from './App.tsx'
-
-const queryClient = new QueryClient();
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>  
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-      <Toaster richColors position="top-right" />
-    </QueryClientProvider>
-  </StrictMode>,
-)
->>>>>>> cab71563bdcfa8c43f5c2a27e33ea37bfeed05ec
