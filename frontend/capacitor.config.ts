4d8411e022fa7be04b72f22583da3db02989ff7b
jgsoto <sotoj9268@gmail.com>
2026-07-04 13:17:40 -0500

    feature(mobile): create icon and splash screen

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.eduprocess',
   appName: 'EduProcess',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffff",
+      androidSplashResourceName: "splash",
+      showSpinner: false
+    }
+  }
 };
 
 export default config;
